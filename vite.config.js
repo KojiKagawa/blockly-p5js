4eb0c66c1d5224a790a645d7a81424b1d5137e54
Koji Kagawa <kagawa.koji@kagawa-u.ac.jp>
2026-04-18 00:24:26 +0900

    treeshake と minify を防ぐための設定を追加

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,22 @@
 
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      treeshake: {
+        // p5 を含むモジュールに対しては side effects があるとみなす
+        moduleSideEffects: (id) => {
+          if (id.includes('p5')) return true;
+          return false;
+        }
+      }
+    },
+    minify: 'terser',
+    terserOptions: {
+      mangle: {
+        // setup と draw は名前を変えないように指定
+        reserved: ['setup', 'draw']
+      }
+    }
+  }
 });
